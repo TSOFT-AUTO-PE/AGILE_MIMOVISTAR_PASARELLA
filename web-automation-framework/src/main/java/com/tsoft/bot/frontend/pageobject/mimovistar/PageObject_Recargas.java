7ec077fedca005f57d93bbf17c16bfc8b6ac490d
TSOFT <abraham_alex_89@outlook.com>
2020-03-06 16:13:02 -0500

    Se sube el primer script para Flujo Recargas - Naimar García

---
+++ web-automation-framework/src/main/java/com/tsoft/bot/frontend/pageobject/mimovistar/PageObject_Recargas.java
@@ -0,0 +1,22 @@
+package com.tsoft.bot.frontend.pageobject.mimovistar;
+
+import org.openqa.selenium.By;
+import org.sikuli.script.Pattern;
+
+public class PageObject_Recargas {
+
+    public static By TXT_NUMERO_MOVISTAR                = By.id("input_celular"); //id
+    public static By TXT_MONTO                          = By.id("input_monto"); //id
+    public static By BTN_CONTINUAR                      = By.id("button_recargar"); //id
+    public static final Pattern TXT_CORREO              = new Pattern("/src/main/resources/sikuli_images/txt_correo.PNG");
+    public static final Pattern BTN_CONTINUAR2          = new Pattern("/src/main/resources/sikuli_images/btn_continuar2.PNG");
+    public static final Pattern TXT_NUMERO_TARJETA      = new Pattern("/src/main/resources/sikuli_images/txt_numtarjeta.PNG");
+    public static final Pattern LIST_FECHVEN            = new Pattern("/src/main/resources/sikuli_images/list_fechven.PNG");
+    public static final Pattern SELECT_MES              = new Pattern("/src/main/resources/sikuli_images/select_mes.PNG");
+    public static final Pattern LIST_ANIO               = new Pattern("/src/main/resources/sikuli_images/list_anio.PNG");
+    public static final Pattern SELECT_ANIO             = new Pattern("/src/main/resources/sikuli_images/select_anio.PNG");
+    public static final Pattern TXT_CVV                 = new Pattern("/src/main/resources/sikuli_images/txt_cvv.PNG");
+    public static final Pattern BTN_PAGAR               = new Pattern("/src/main/resources/sikuli_images/btn_pagar.PNG");
+    public static String LBL_EXITO                      = "/html/body/app-root/app-confirmation/div/div[1]/div[2]/label";
+    public static By TXT_CORREO2                        = By.cssSelector("#mat-input-0");
+}
---
+++ web-automation-framework/target/cucumber/report.js
@@ -0,0 +1,286 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("MiMovistar_FlujoRecargas.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Mi Movistar Flujo de Recargas",
+  "description": "",
+  "id": "mi-movistar-flujo-de-recargas",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "line": 24,
+  "name": "Recargas en Mi Movistar",
+  "description": "",
+  "id": "mi-movistar-flujo-de-recargas;recargas-en-mi-movistar",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 23,
+      "name": "@MiMovistar_Recargas_Completo"
+    }
+  ]
+});
+formatter.step({
+  "line": 26,
+  "name": "se genera la URL mediante el API_TEST_recargas",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 27,
+  "name": "se ingresa en la URL el token generado \"\u003ccaso_prueba\u003e\"",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 28,
+  "name": "se ingresa el numero celular \"\u003ccaso_prueba\u003e\"",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 29,
+  "name": "monto a recargar \"\u003ccaso_prueba\u003e\"",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 30,
+  "name": "se da click en el boton Continuar",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 31,
+  "name": "se ingresa el email \"\u003ccaso_prueba\u003e\" y se da click en continuar",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 32,
+  "name": "se ingresa el numero de tarjeta \"\u003ccaso_prueba\u003e\"",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 33,
+  "name": "se ingresa el mes de vencimiento \"\u003ccaso_prueba\u003e\"",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 34,
+  "name": "se ingresa el anio de vencimiento \"\u003ccaso_prueba\u003e\"",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 35,
+  "name": "se ingresa el codigo de verificacion \"\u003ccaso_prueba\u003e\"",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 36,
+  "name": "se da click en el boton Pagar",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 37,
+  "name": "se verifica que se hizo la recarga correcta.",
+  "keyword": "Then "
+});
+formatter.examples({
+  "line": 39,
+  "name": "",
+  "description": "",
+  "id": "mi-movistar-flujo-de-recargas;recargas-en-mi-movistar;",
+  "rows": [
+    {
+      "cells": [
+        "caso_prueba"
+      ],
+      "line": 40,
+      "id": "mi-movistar-flujo-de-recargas;recargas-en-mi-movistar;;1"
+    },
+    {
+      "cells": [
+        "1"
+      ],
+      "line": 41,
+      "id": "mi-movistar-flujo-de-recargas;recargas-en-mi-movistar;;2"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.before({
+  "duration": 169490900,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 10632543800,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 41,
+  "name": "Recargas en Mi Movistar",
+  "description": "",
+  "id": "mi-movistar-flujo-de-recargas;recargas-en-mi-movistar;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 23,
+      "name": "@MiMovistar_Recargas_Completo"
+    }
+  ]
+});
+formatter.step({
+  "line": 26,
+  "name": "se genera la URL mediante el API_TEST_recargas",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 27,
+  "name": "se ingresa en la URL el token generado \"1\"",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 28,
+  "name": "se ingresa el numero celular \"1\"",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 29,
+  "name": "monto a recargar \"1\"",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 30,
+  "name": "se da click en el boton Continuar",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 31,
+  "name": "se ingresa el email \"1\" y se da click en continuar",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 32,
+  "name": "se ingresa el numero de tarjeta \"1\"",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 33,
+  "name": "se ingresa el mes de vencimiento \"1\"",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 34,
+  "name": "se ingresa el anio de vencimiento \"1\"",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 35,
+  "name": "se ingresa el codigo de verificacion \"1\"",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 36,
+  "name": "se da click en el boton Pagar",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 37,
+  "name": "se verifica que se hizo la recarga correcta.",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "MiMovistar_Recargas.seGeneraLaURLMedianteElAPI_TEST_recargas()"
+});
+formatter.result({
+  "duration": 3574501000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "1",
+      "offset": 40
+    }
+  ],
+  "location": "MiMovistar_Recargas.seIngresaEnLaURLElTokenGenerado(String)"
+});
+formatter.result({
+  "duration": 8830871900,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "1",
+      "offset": 30
+    }
+  ],
+  "location": "MiMovistar_Recargas.seIngresaElNumeroCelular(String)"
+});
+formatter.result({
+  "duration": 1406407100,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "1",
+      "offset": 18
+    }
+  ],
+  "location": "MiMovistar_Recargas.montoARecargar(String)"
+});
+formatter.result({
+  "duration": 723982300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MiMovistar_Recargas.seDaClickEnElBotonContinuar()"
+});
+formatter.result({
+  "duration": 4648209000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "1",
+      "offset": 21
+    }
+  ],
+  "location": "MiMovistar_Recargas.seIngresaElEmailYSeDaClickEnContinuar(String)"
+});
+formatter.result({
+  "duration": 5596183500,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "1",
+      "offset": 33
+    }
+  ],
+  "location": "MiMovistar_Recargas.seIngresaElNumeroDeTarjeta(String)"
+});
